f5b1e6642d4b3f9a41fe9a0e4120f3d45de32adf
QRYPTEC <64428384+QRYPTEC@users.noreply.github.com>
2020-04-27 16:33:10 +0000

    Update dor.php

--- dor.php
+++ dor.php
@@ -1,5 +1,25 @@
 <?php
 
+
+
+
+$imei = "862849043303718";
+echo "Masukkan No Telepon/HP : ";
+$msisdn = trim(fgets(STDIN));
+$otp = $tri->request_otp($msisdn,$imei);
+echo $otp[1] . "\r\n";
+echo "Masukkan OTP : ";
+$otp = trim(fgets(STDIN));
+$login = $tri->login($msisdn,$otp);
+$login = json_decode($login,true);
+$bearer = $login['access_token'];
+$id = $tri->trans($bearer);
+$id = json_decode($id,true);
+$id = $id['data'][0]['rewardTransactionId'];
+for($id1 = 1500; $id1 < 1600;$id1++)
+{
+  <?php
+
 include 'hajar.php';
 
 $tri = new tri();
@@ -18,8 +38,8 @@ $id = json_decode($id,true);
 $id = $id['data'][0]['rewardTransactionId'];
 for($id1 = 1500; $id1 < 1600;$id1++)
 {
-  $gas = $tri->claim($bearer,$id,$id1);
-  echo $gas . "\r\n";
+  $dor = $tri->claim($bearer,$id,$id1);
+  echo $dor . "\r\n";
   sleep(2);
 }
 
